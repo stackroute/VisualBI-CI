5af8b704dc4327171525583a8ab193ad12a99d65
abhi1623 <abhilashk.iit@gmail.com>
2016-01-27 14:33:22 +0000

    added license

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var dbURI = "mongodb://localhost/visualBI2"; //"mongodb://172.23.238.253/visualBI";
+var dbURI = "mongodb://172.23.238.253/visualBI";
 mongoose.connect(dbURI);
 
 mongoose.connection.on('connected', function () {
